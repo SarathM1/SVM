e0ef48643a7ba4930676524e8869ea547bdf79a4
Office PC <sarathm00@gmail.com>
2016-04-29 16:03:52 +0530

    Cleaned up the code

--- plotMap.py
+++ plotMap.py
@@ -10,7 +10,6 @@ from sklearn import svm, metrics
 """from sklearn.datasets.base import Bunch
 from sklearn import svm, metrics"""
 
-species=("bradypus_variegatus_0", "microryzomys_minutus_0")
 def create_species_bunch(species_name, train, test, coverages, xgrid, ygrid):
     """Create a bunch with information about a particular organism
 
@@ -33,7 +32,7 @@ def create_species_bunch(species_name, train, test, coverages, xgrid, ygrid):
 
     return bunch
 
-def draw_map(data, xgrid, ygrid, X, Y, land_reference):
+def draw_map(input_data, xgrid, ygrid, X, Y, land_reference):
     # Plot map of South America"""
     plt.subplot(1, 1, 1)
     print(" - plot coastlines from coverage")
@@ -41,35 +40,35 @@ def draw_map(data, xgrid, ygrid, X, Y, land_reference):
             levels=[-9999], colors="k",
             linestyles="solid")
 
-def SVM_fun(data, land_reference, mean, std):
+def SVM_fun(input_data, land_reference, mean, std, data):
     # Fit OneClassSVM
-    train_cover_std = (species.cov_train - mean) / std
+    train_cover_std = (data.cov_train - mean) / std
     print " - fit OneClassSVM ... "
     clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.5)
     clf.fit(train_cover_std)
     print "done"
 
-    print(" - predict species distribution")
+    print(" - predict data distribution")
 
-    # Predict species distribution using the training data
+    # Predict data distribution using the training data
 
     # We'll predict only for the land points.
     idx = np.where(land_reference > -9999)
-    coverages_land = data.coverages[:, idx[0], idx[1]].T
+    coverages_land = input_data.coverages[:, idx[0], idx[1]].T
 
     pred = clf.decision_function((coverages_land - mean) / std)[:, 0]
     
     return pred, clf, idx
 
-def compute_AUC(Z, data, clf, mean, std):
+def compute_AUC(Z, input_data, clf, mean, std, data):
     # Compute AUC with regards to background points
     np.random.seed(13)
-    background_points = np.c_[np.random.randint(low=0, high=data.Ny,
+    background_points = np.c_[np.random.randint(low=0, high=input_data.Ny,
                                                 size=10000),
-                              np.random.randint(low=0, high=data.Nx,
+                              np.random.randint(low=0, high=input_data.Nx,
                                                 size=10000)].T
     pred_background = Z[background_points[0], background_points[1]]
-    pred_test = clf.decision_function((species.cov_test - mean)
+    pred_test = clf.decision_function((data.cov_test - mean)
                                         / std)[:, 0]
     scores = np.r_[pred_test, pred_background]
     y = np.r_[np.ones(pred_test.shape), np.zeros(pred_background.shape)]
@@ -83,37 +82,36 @@ def mark_Prediction(X, Y, Z, levels):
     plt.contourf(X, Y, Z, levels=levels, cmap=plt.cm.Reds)
     plt.colorbar(format='%.2f')
 
-def mark_Points(species):
+def mark_Points(data):
     # scatter training/testing points
-    plt.scatter(species.pts_train['dd long'], species.pts_train['dd lat'],
+    plt.scatter(data.pts_train['dd long'], data.pts_train['dd lat'],
                 s=2 ** 2, c='black',
                 marker='^', label='train')
-    plt.scatter(species.pts_test['dd long'], species.pts_test['dd lat'],
+    plt.scatter(data.pts_test['dd long'], data.pts_test['dd lat'],
                 s=2 ** 2, c='black',
                 marker='x', label='test')
 def main():
-    global species
     # Extracting Data from web
-    data = fetch_species_distributions()
-    xgrid, ygrid = construct_grids(data)
+    input_data = fetch_species_distributions()
+    xgrid, ygrid = construct_grids(input_data)
     X, Y = np.meshgrid(xgrid, ygrid[::-1])
-    land_reference = data.coverages[6]
-    BV_bunch = create_species_bunch(species[0],
-                                    data.train, data.test,
-                                    data.coverages, xgrid, ygrid)
+    land_reference = input_data.coverages[6]
+    BV_bunch = create_species_bunch("bradypus_variegatus_0",
+                                    input_data.train, input_data.test,
+                                    input_data.coverages, xgrid, ygrid)
 
-    draw_map(data, xgrid, ygrid, X, Y, land_reference)
+    draw_map(input_data, xgrid, ygrid, X, Y, land_reference)
     plt.xticks([])
     plt.yticks([])
 
-    for i, species in enumerate([BV_bunch]):
+    for i, data in enumerate([BV_bunch]):
         # Standardize features
-        mean = species.cov_train.mean(axis=0)
-        std = species.cov_train.std(axis=0)
+        mean = data.cov_train.mean(axis=0)
+        std = data.cov_train.std(axis=0)
         
-        pred, clf, idx = SVM_fun(data, land_reference, mean, std)
+        pred, clf, idx = SVM_fun(input_data, land_reference, mean, std, data)
   
-        Z = np.ones((data.Ny, data.Nx), dtype=np.float64)
+        Z = np.ones((input_data.Ny, input_data.Nx), dtype=np.float64)
         Z *= pred.min()
         Z[idx[0], idx[1]] = pred
 
@@ -122,13 +120,13 @@ def main():
 
         mark_Prediction(X, Y, Z, levels)
 
-        mark_Points(species)
+        mark_Points(data)
         
         plt.legend()
         plt.title('Title')
         plt.axis('equal')
         
-        compute_AUC(Z, data, clf, mean, std)
+        compute_AUC(Z, input_data, clf, mean, std, data)
     plt.show()
 if __name__ == "__main__":
     main()
