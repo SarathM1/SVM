a8d3d7ae41f16866383bc02ab32c86dec5f3af39
Office PC <sarathm00@gmail.com>
2016-04-29 13:20:14 +0530

    Removed unnecessary lines

--- plotMap.py
+++ plotMap.py
@@ -2,27 +2,14 @@ from time import time
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-from sklearn.datasets.base import Bunch
 from sklearn.datasets import fetch_species_distributions
 from sklearn.datasets.species_distributions import construct_grids
-from sklearn import svm, metrics
 
-species=("bradypus_variegatus_0", "microryzomys_minutus_0")
+"""from sklearn.datasets.base import Bunch
+from sklearn import svm, metrics"""
+
 # Extracting Data from web
 data = fetch_species_distributions()
-
-# Print all the subsection available
-
-print "keys = ", data.keys()
-print "\n\n Hit Enter to Continue"
-raw_input()
-
-print "_"*80
-print "Data = "
-print data['coverages'][6]
-print "_"*80
-
 
 land_reference = data.coverages[6]
 
@@ -33,7 +20,7 @@ xgrid, ygrid = construct_grids(data)
 X, Y = np.meshgrid(xgrid, ygrid[::-1])
 # Plot map of South America"""
 # for i, species in enumerate([BV_bunch, MM_bunch]):
-plt.subplot(1, 2, 1)
+plt.subplot(1, 1, 1)
 print(" - plot coastlines from coverage")
 plt.contour(X, Y, land_reference,
         levels=[-9999], colors="k",
