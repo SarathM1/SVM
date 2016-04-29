4ec1ee8088ed5cd3837859562a570b4f44fe32e9
Laptop <sarathm00@gmail.com>
2016-04-30 00:47:16 +0530

    Code to write all data to binary file

--- dataset.py
+++ dataset.py
@@ -1,15 +1,47 @@
-from sklearn.datasets import fetch_species_distributions 
+from sklearn.datasets import fetch_species_distributions
+from sklearn.datasets.species_distributions import construct_grids
+import numpy as np
+
+
+def save_to_file(file_name, data1, data2, data3, data4, data5, data6, data7, data8):
+    f = file(file_name, "wb")
+    np.save(f, data1)
+    np.save(f, data2)
+    np.save(f, data3)
+    np.save(f, data4)
+    np.save(f, data5)
+    np.save(f, data6)
+    np.save(f, data7)
+    np.save(f, data8)
+    f.close()
+
+
+def load_from_file(file_name):
+    f = file(file_name, "rb")
+    data1 = np.load(f)
+    data2 = np.load(f)
+    data3 = np.load(f)
+    data4 = np.load(f)
+    data5 = np.load(f)
+    data6 = np.load(f)
+    data7 = np.load(f)
+    data8 = np.load(f)
+    f.close()
+    return data1, data2, data3, data4, data5, data6, data7, data8
 
 # Extracting Data from web
 data = fetch_species_distributions()
 
 # Print all the subsection available
-
 print "keys = ", data.keys()
-print "\n\n Hit Enter to Continue"
-raw_input()
 
 print "Data = "
 print "_"*80
-print data
+xgrid, ygrid = construct_grids(data)
+save_to_file("database.bin", xgrid, ygrid, data.coverages[6], data.coverages, data.test, data.train, data.Nx, data.Ny)
+print len(xgrid), len(ygrid), len(data.coverages[6]), len(data.coverages), len(data.test), len(data.train), data.Nx, data.Ny
+print "_"*80
+
+a, b, c, d, e, f, g, h = load_from_file("database.bin")
+print len(a), len(b), len(c), len(d), len(e), len(f), g, h
 print "_"*80
