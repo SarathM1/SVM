11340e280b7aab89a290be339c4d86a68290e771
Laptop <sarathm00@gmail.com>
2016-04-30 01:38:47 +0530

    Added code to rename species to patient

--- dataset.py
+++ dataset.py
@@ -29,6 +29,20 @@ def load_from_file(file_name):
     f.close()
     return data1, data2, data3, data4, data5, data6, data7, data8
 
+
+def replace(data):
+    x = []
+    x.append(data[0][0])
+    for i in range(len(data)):
+        if data[i][0] not in x:
+            x.append(data[i][0])
+            # print data[i][0]
+    for i in range(20):
+        for j in range(len(data)):
+            if data[j][0] == x[i]:
+                # print data[j][0], '=>', 'patient_' + str(i)
+                data[j][0] = 'patient_' + str(i)
+
 # Extracting Data from web
 data = fetch_species_distributions()
 
@@ -38,6 +52,8 @@ print "keys = ", data.keys()
 print "Data = "
 print "_"*80
 xgrid, ygrid = construct_grids(data)
+replace(data.test)
+replace(data.train)
 save_to_file("database.bin", xgrid, ygrid, data.coverages[6], data.coverages, data.test, data.train, data.Nx, data.Ny)
 print len(xgrid), len(ygrid), len(data.coverages[6]), len(data.coverages), len(data.test), len(data.train), data.Nx, data.Ny
 print "_"*80
