a2ad80dd1d98e5db6159794bbb465c8580f30305
Office PC <sarathm00@gmail.com>
2016-04-29 14:30:29 +0530

    Final plot

--- plotMap.py
+++ plotMap.py
@@ -4,25 +4,121 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_species_distributions
 from sklearn.datasets.species_distributions import construct_grids
+from sklearn.datasets.base import Bunch
+from sklearn import svm, metrics
 
 """from sklearn.datasets.base import Bunch
 from sklearn import svm, metrics"""
 
-# Extracting Data from web
-data = fetch_species_distributions()
+species=("bradypus_variegatus_0", "microryzomys_minutus_0")
+def create_species_bunch(species_name, train, test, coverages, xgrid, ygrid):
+    """Create a bunch with information about a particular organism
 
-land_reference = data.coverages[6]
+    This will use the test/train record arrays to extract the
+    data specific to the given species name.
+    """
+    bunch = Bunch(name=' '.join(species_name.split("_")[:2]))
+    species_name = species_name.encode('ascii')
+    points = dict(test=test, train=train)
 
-# Set up the data grid
-xgrid, ygrid = construct_grids(data)
+    for label, pts in points.items():
+        # choose points associated with the desired species
+        pts = pts[pts['species'] == species_name]
+        bunch['pts_%s' % label] = pts
 
-# The grid in x,y coordinates
-X, Y = np.meshgrid(xgrid, ygrid[::-1])
-# Plot map of South America"""
-# for i, species in enumerate([BV_bunch, MM_bunch]):
-plt.subplot(1, 1, 1)
-print(" - plot coastlines from coverage")
-plt.contour(X, Y, land_reference,
-        levels=[-9999], colors="k",
-        linestyles="solid")
-plt.show()
+        # determine coverage values for each of the training & testing points
+        ix = np.searchsorted(xgrid, pts['dd long'])
+        iy = np.searchsorted(ygrid, pts['dd lat'])
+        bunch['cov_%s' % label] = coverages[:, -iy, ix].T
+
+    return bunch
+
+def draw_map(data):
+    land_reference = data.coverages[6]
+
+    # Set up the data grid
+    xgrid, ygrid = construct_grids(data)
+
+    # The grid in x,y coordinates
+    X, Y = np.meshgrid(xgrid, ygrid[::-1])
+    # Plot map of South America"""
+    plt.subplot(1, 1, 1)
+    print(" - plot coastlines from coverage")
+    plt.contour(X, Y, land_reference,
+            levels=[-9999], colors="k",
+            linestyles="solid")
+
+def main():
+    global species
+    # Extracting Data from web
+    data = fetch_species_distributions()
+    xgrid, ygrid = construct_grids(data)
+    X, Y = np.meshgrid(xgrid, ygrid[::-1])
+    land_reference = data.coverages[6]
+    background_points = np.c_[np.random.randint(low=0, high=data.Ny,
+                                                size=10000),
+                              np.random.randint(low=0, high=data.Nx,
+                                                size=10000)].T
+    BV_bunch = create_species_bunch(species[0],
+                                    data.train, data.test,
+                                    data.coverages, xgrid, ygrid)
+
+    draw_map(data)
+
+    for i, species in enumerate([BV_bunch]):
+        # Standardize features
+        mean = species.cov_train.mean(axis=0)
+        std = species.cov_train.std(axis=0)
+        train_cover_std = (species.cov_train - mean) / std
+        
+        # Fit OneClassSVM
+        print " - fit OneClassSVM ... "
+        clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.5)
+        clf.fit(train_cover_std)
+        print "done"
+
+        print(" - predict species distribution")
+
+        # Predict species distribution using the training data
+        Z = np.ones((data.Ny, data.Nx), dtype=np.float64)
+
+        # We'll predict only for the land points.
+        idx = np.where(land_reference > -9999)
+        coverages_land = data.coverages[:, idx[0], idx[1]].T
+
+        pred = clf.decision_function((coverages_land - mean) / std)[:, 0]
+        Z *= pred.min()
+        Z[idx[0], idx[1]] = pred
+
+        levels = np.linspace(Z.min(), Z.max(), 25)
+        Z[land_reference == -9999] = -9999
+
+        # plot contours of the prediction
+        plt.contourf(X, Y, Z, levels=levels, cmap=plt.cm.Reds)
+        plt.colorbar(format='%.2f')
+
+        # scatter training/testing points
+        plt.scatter(species.pts_train['dd long'], species.pts_train['dd lat'],
+                    s=2 ** 2, c='black',
+                    marker='^', label='train')
+        plt.scatter(species.pts_test['dd long'], species.pts_test['dd lat'],
+                    s=2 ** 2, c='black',
+                    marker='x', label='test')
+        plt.legend()
+        plt.title(species.name)
+        plt.axis('equal')
+        
+        # Compute AUC with regards to background points
+        pred_background = Z[background_points[0], background_points[1]]
+        pred_test = clf.decision_function((species.cov_test - mean)
+                                          / std)[:, 0]
+        scores = np.r_[pred_test, pred_background]
+        y = np.r_[np.ones(pred_test.shape), np.zeros(pred_background.shape)]
+        fpr, tpr, thresholds = metrics.roc_curve(y, scores)
+        roc_auc = metrics.auc(fpr, tpr)
+        plt.text(-35, -70, "AUC: %.3f" % roc_auc, ha="right")
+        print("\n Area under the ROC curve : %f" % roc_auc)
+    plt.show()
+if __name__ == "__main__":
+    main()
+
