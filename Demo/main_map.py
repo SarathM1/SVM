8aa2b02d17b41bff4b3cb044994da7eb55981c39
Laptop <sarathm00@gmail.com>
2016-05-01 22:39:59 +0530

    Deleted unnecessary lines

--- Demo/main_map.py
+++ Demo/main_map.py
@@ -1,33 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets.base import Bunch
-from sklearn import svm, metrics
 
 """from sklearn.datasets.base import Bunch
 from sklearn import svm, metrics"""
 
 DISEASE_NAME = 'disease_0'
-def create_species_bunch(species_name, train, test, coverages, xgrid, ygrid):
-    """Create a bunch with information about a particular organism
-
-    This will use the test/train record arrays to extract the
-    data specific to the given species name.
-    """
-    bunch = Bunch(name=' '.join(species_name.split("_")[:2]))
-    species_name = species_name.encode('ascii')
-    points = dict(test=test, train=train)
-
-    for label, pts in points.items():
-        # choose points associated with the desired species
-        pts = pts[pts['species'] == species_name]
-        bunch['pts_%s' % label] = pts
-
-        # determine coverage values for each of the training & testing points
-        ix = np.searchsorted(xgrid, pts['dd long'])
-        iy = np.searchsorted(ygrid, pts['dd lat'])
-        bunch['cov_%s' % label] = coverages[:, -iy, ix].T
-
-    return bunch
 
 
 def draw_map(X, Y, land_reference):
@@ -36,60 +13,6 @@ def draw_map(X, Y, land_reference):
     print(" - plot coastlines from coverage")
     plt.contour(X, Y, land_reference, levels=[-9999], colors="k",
                 linestyles="solid")
-
-
-def SVM_fun(coverage_data, land_reference, mean, std, data):
-    # Fit OneClassSVM
-    train_cover_std = (data.cov_train - mean) / std
-    print " - fit OneClassSVM ... "
-    clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.5)
-    clf.fit(train_cover_std)
-    print "done"
-
-    print(" - predict data distribution")
-
-    # Predict data distribution using the training data
-
-    # We'll predict only for the land points.
-    idx = np.where(land_reference > -9999)
-    coverages_land = coverage_data[:, idx[0], idx[1]].T
-
-    pred = clf.decision_function((coverages_land - mean) / std)[:, 0]
-
-    return pred, clf, idx
-
-
-def compute_AUC(Z, clf, mean, std, data, Nx_data, Ny_data):
-    # Compute AUC with regards to background points
-    np.random.seed(13)
-    background_points = np.c_[np.random.randint(low=0, high=Ny_data,
-                                                size=10000),
-                              np.random.randint(low=0, high=Nx_data,
-                                                size=10000)].T
-    pred_background = Z[background_points[0], background_points[1]]
-    pred_test = clf.decision_function((data.cov_test - mean) / std)[:, 0]
-    scores = np.r_[pred_test, pred_background]
-    y = np.r_[np.ones(pred_test.shape), np.zeros(pred_background.shape)]
-    fpr, tpr, thresholds = metrics.roc_curve(y, scores)
-    roc_auc = metrics.auc(fpr, tpr)
-    plt.text(-35, -70, "AUC: %.3f" % roc_auc, ha="right")
-    print("\n Area under the ROC curve : %f" % roc_auc)
-
-
-def mark_Prediction(X, Y, Z, levels):
-    # plot contours of the prediction
-    plt.contourf(X, Y, Z, levels=levels, cmap=plt.cm.Reds)
-    plt.colorbar(format='%.2f')
-
-
-def mark_Points(data):
-    # scatter training/testing points
-    plt.scatter(data.pts_train['dd long'], data.pts_train['dd lat'],
-                s=2 ** 2, c='black',
-                marker='^', label='train')
-    plt.scatter(data.pts_test['dd long'], data.pts_test['dd lat'],
-                s=2 ** 2, c='black',
-                marker='x', label='test')
 
 
 def load_from_file(file_name):
